928d718876f991aa52611115e73dbb8e325bd3d7
Jingwen Owen Ou <jingweno@gmail.com>
2010-02-12 04:11:50 -0800

    fixed production setting

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,1 +1,7 @@
-MongoMapper.database = "ijws-#{Rails.env}"
+db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
+ 
+if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
+  mongo = db_config[Rails.env]
+  MongoMapper.connection = Mongo::Connection.new(mongo['hostname'], mongo['port'])
+  MongoMapper.database = mongo['database']
+end
