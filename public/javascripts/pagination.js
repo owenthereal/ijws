9d25ebef241cd7c30142a62351e1e586625b1a5a
Jingwen Owen Ou <jingweno@gmail.com>
2010-04-08 22:50:35 -0700

    added loading page latency messages

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,6 +1,6 @@
 $(function () {  
     $('#pagination > li > a').live("click", function () {  
-  		$('.pagination').html('Loading...');
+  		$('#pagination').html('Loading...');
 		$.get(this.href, null, null, 'script');  
         return false;  
     });  
